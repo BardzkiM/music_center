f54e1357e65ec8c8c8f497c6c7d658f29fe2ee26
Michał Bardzki <m.bardzki@getprintbox.com>
2017-10-11 20:36:43 +0200

    main structure scss

--- app/components/layout/MainLayout.js
+++ app/components/layout/MainLayout.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from './header/Header'
+import Content from './content/Content'
 
 export default () => {
   return (
     <div className="MainLayout">
-
+      <Header/>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
