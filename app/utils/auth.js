becdacf615d3766cdfda56ec24229c55a12f5dc9
Piotr <piotr1_krk@interia.pl>
2017-10-27 21:03:59 +0200

    constant value

--- app/utils/auth.js
+++ app/utils/auth.js
@@ -1,10 +1,11 @@
 import {getIsUserLogged} from '../modules/userData/selectors';
+import {API} from '../constants';
 
 export function requireAuth(nextState, replace) {
   const {store: {getState, dispatch}} = this.props;
   const isLogged = getIsUserLogged(getState());
 
   if (!isLogged) {
-    replace('/login');
+    replace(API.login);
   }
 }
