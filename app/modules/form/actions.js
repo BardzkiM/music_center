616da8a736026aebfc6bed1561bc3ffd1334bab5
Piotr <piotr1_krk@interia.pl>
2017-10-29 17:15:06 +0100

    additem ui fix

--- app/modules/form/actions.js
+++ app/modules/form/actions.js
@@ -1,6 +1,6 @@
 import {createAction} from 'redux-actions';
 
 export const REQUEST_DATA = createAction('FORM/REQUEST_DATA', () => null);
-export const REQUEST_DATA_SUCCESS = createAction('FORM/REQUEST_DATA_SUCCESS', () => null);
+export const REQUEST_DATA_SUCCESS = createAction('FORM/REQUEST_DATA_SUCCESS', data => data);
 export const REQUEST_DATA_FAILED = createAction('FORM/REQUEST_DATA_FAILED', e => e);
 export const CLEAR_STATUS = createAction('FORM/CLEAR_STATUS', () => null);
